28a001c508c13c6507bc6c599aaf6ecf53973be6
Bhamdou <qgubb21@gmail.com>
2022-08-26 11:14:25 +0200

    first commit

---
+++ ex9.py
@@ -0,0 +1,13 @@
+# def reverse_functinon():
+#     content = input("only usd uppercase or lowercase:")
+#     if content.isupper() == True:
+#         print(content.capitalize()+"!")
+#     elif content == content.capitalize():
+#         print(content.capitalize())
+#         print("option2")
+#     else:
+#         print(content.capitalize())
+        
+        
+# reverse_functinon()    
+    
